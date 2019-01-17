cf57b2350a95cb8d843242171d5c3eda37dbaeb7
Grayson Gilmore <gilmoreg@live.com>
2019-01-16 21:55:08 -0800

    Add segmentation algorithm

---
+++ server/kagome.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"github.com/ikawaha/kagome/tokenizer"
+)
+
+// Features is [0-5] POS, [6] base form, [7] reading, [8] pronounciation
+// 一段 = ichidan, 一般 = common
+
+// JSONToken - json friendly representation of tokenizer.Token
+type JSONToken struct {
+	ID      int      `json:"id"`
+	Class   string   `json:"class"` // DUMMY, KNOWN, UNKNOWN, USER
+	Surface string   `json:"surface"`
+	POS     []string `json:"pos"`
+	Base    string   `json:"base"`
+	Reading string   `json:"reading"`
+	Pron    string   `json:"pron"`
+}
+
+// Word - one or more JSONTokens
+type Word []JSONToken
+
+func newToken(t tokenizer.Token) JSONToken {
+	features := t.Features()
+	result := JSONToken{
+		ID:      t.ID,
+		Class:   t.Class.String(),
+		Surface: t.Surface,
+		POS:     features[0:5],
+		Base:    features[6],
+	}
+	if len(features) > 7 {
+		result.Reading = features[7]
+	}
+	if len(features) > 8 {
+		result.Pron = features[8]
+	}
+	return result
+}
+
+func isPunctuation(pos []string) bool {
+	if len(pos) < 2 {
+		return false
+	}
+	return pos[0] == "記号" && pos[1] == "句点"
+}
+
+func segment(tokens []tokenizer.Token) []Word {
+	words := make([]Word, 0)
+	word := make([]JSONToken, 0)
+	for _, token := range tokens {
+		if token.Class == tokenizer.DUMMY { // BOS and EOS
+			continue
+		}
+		jToken := newToken(token)
+
+		// If we are at a punctuation mark or
+		// a word whose base is equal to its surface
+		// (meaning it is not a conjugated helper)
+		// add current word to words and start a new word
+		if isPunctuation(jToken.POS) {
+			// Finish adding word in progress, if any
+			if len(word) > 0 {
+				words = append(words, word)
+			}
+			// Add punctuation as its own word
+			words = append(words, []JSONToken{jToken})
+			// Start a new word
+			word = make([]JSONToken, 0)
+		} else if jToken.Surface == jToken.Base {
+			word = append(word, jToken)
+			words = append(words, word)
+			// Start a new word
+			word = make([]JSONToken, 0)
+		} else {
+			// We aren't finished - add token to current word
+			word = append(word, jToken)
+		}
+	}
+	// Finish up word in progress, if any
+	if len(word) > 0 {
+		words = append(words, word)
+	}
+	return words
+}
+
+// Tokenize - use Kagome to tokenize input string, collect into words
+func Tokenize(query string) []Word {
+	t := tokenizer.New()
+	tokens := t.Analyze(query, tokenizer.Search)
+	words := segment(tokens)
+	return words
+}
--- server/main.go
+++ server/main.go
@@ -2,63 +2,41 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
-	"github.com/ikawaha/kagome/tokenizer"
 )
 
-// Features is [0-5] POS, [6] base form, [7] reading, [8] pronounciation
-// 一段 = ichidan, 一般 = common
-
-type jsonToken struct {
-	ID      int      `json:"id"`
-	Class   string   `json:"class"` // DUMMY, KNOWN, UNKNOWN, USER
-	Surface string   `json:"surface"`
-	POS     []string `json:"pos"`
-	Base    string   `json:"base"`
-	Reading string   `json:"reading"`
-	Pron    string   `json:"pron"`
-}
-
-func newToken(t tokenizer.Token) jsonToken {
-	features := t.Features()
-	result := jsonToken{
-		ID:      t.ID,
-		Class:   t.Class.String(),
-		Surface: t.Surface,
-		POS:     features[0:5],
-		Base:    features[6],
-		Reading: features[7],
-		Pron:    features[8],
-	}
-	return result
+type postBody struct {
+	Query string `json:"query"`
 }
 
 func handler(rw http.ResponseWriter, r *http.Request) {
-	t := tokenizer.New()
-	tokens := t.Analyze("寿司が食べたい。", tokenizer.Search)
-	jTokens := make([]jsonToken, 0)
-	for _, token := range tokens {
-		if token.Class == tokenizer.DUMMY { // BOS and EOS
-			continue
-		}
-		jToken := newToken(token)
-		jTokens = append(jTokens, jToken)
-	}
-	if j, err := json.Marshal(jTokens); err != nil {
+	var body postBody
+	json.NewDecoder(r.Body).Decode(&body)
+	if body.Query == "" {
+		fmt.Println("could not decode post body")
 		http.Error(rw, http.StatusText(500), 500)
 		return
-	} else {
-		rw.Write(j)
 	}
+	query := body.Query
+	tokens := Tokenize(query)
+	j, err := json.Marshal(tokens)
+	if err != nil {
+		fmt.Println(err)
+		http.Error(rw, http.StatusText(500), 500)
+		return
+	}
+	rw.Header().Set("Content-Type", "application/json")
+	rw.Write(j)
 }
 
 func main() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", handler)
+	r.HandleFunc("/", handler).Methods("POST")
 	loggedRouter := handlers.LoggingHandler(os.Stdout, r)
 	http.ListenAndServe(":3000", loggedRouter)
 }
