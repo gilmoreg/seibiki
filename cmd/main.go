995ef8c5b19a337feaa82d8b07affb5d65278d4a
Grayson Gilmore <gilmoreg@live.com>
2019-02-23 09:48:35 -0800

    Go-kit refactor

--- cmd/main.go
+++ cmd/main.go
@@ -9,21 +9,20 @@ import (
 	"github.com/gilmoreg/seibiki/internal/connectors/redis"
 	"github.com/gilmoreg/seibiki/internal/dictionary"
 	"github.com/gilmoreg/seibiki/internal/endpoints"
-	"github.com/gilmoreg/seibiki/internal/service"
 	"github.com/gorilla/mux"
 	"go.uber.org/zap"
 )
 
 // Server - holds deps for injection
 type Server struct {
-	svc    service.LookupService
-	router *mux.Router
-	logger *zap.SugaredLogger
+	dictionary dictionary.Repository
+	router     *mux.Router
+	logger     *zap.SugaredLogger
 }
 
 // Routes - add routes
 func (s *Server) Routes() {
-	s.router.Path("/api/lookup").Methods("POST").Handler(endpoints.Handler(s.svc, s.logger))
+	s.router.Path("/api/lookup").Methods("POST").Handler(endpoints.Handler(s.logger))
 	s.router.
 		PathPrefix("/static/js/").
 		Handler(http.StripPrefix("/static/js/", http.FileServer(http.Dir("/go/bin/wwwroot/static/js/"))))
@@ -40,14 +39,14 @@ func main() {
 	l := zap.NewExample().Sugar()
 	defer l.Sync()
 	r := mux.NewRouter()
-	c := redis.New(l)
-	m := mongodb.New(os.Getenv("MONGODB_CONNECTION_STRING"), l)
-	d := dictionary.New(m, c, l)
-	svc := service.New(l, d)
+	c := redis.New()
+	m := mongodb.
+	d := dictionary.New()
+	m.Connect(os.Getenv("MONGODB_CONNECTION_STRING"))
 	s := Server{
-		router: r,
-		svc:    svc,
-		logger: l,
+		router:     r,
+		dictionary: m,
+		logger:     l,
 	}
 	s.Routes()
 	url := fmt.Sprintf(":%s", os.Getenv("PORT"))
