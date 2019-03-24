5773dcdd362100fa0fee5926951718f557e6c0d9
Grayson Gilmore <gilmoreg@live.com>
2019-03-24 08:42:04 -0700

    Test ci fix

--- build/data/seed.sh
+++ build/data/seed.sh
@@ -1,2 +1,2 @@
 pwd
-mongorestore --host localhost --port 27017 --drop --gzip --archive=./jedict.mongodb.archive
+mongorestore --host localhost --port 27017 --drop --gzip --archive=./build/data/jedict.mongodb.archive
